692ca985663fa307895a5109f6acd54c7b0091b2
Valarie Adams <deviantdear@gmail.com>
2019-02-06 00:16:44 -0700

    Started some fundamental player scripts + slow motion testing
    also re-added the missing folders

---
+++ CPI211/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private bool isWalking;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+    public CharacterController characterController;
+
+}
