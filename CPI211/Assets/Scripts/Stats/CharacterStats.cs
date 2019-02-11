9374f3a3ec4a5715df201564d45298fd0c7619aa
Valarie Adams <deviantdear@gmail.com>
2019-02-11 00:22:02 -0700

    Created Enemy and Player Health Stats as well as updated Throw Object Script

---
+++ CPI211/Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+
+    // Health
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+
+    public Stat damage;
+
+    // Set current health to max health
+    // when starting the game.
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Damage the character
+    public void TakeDamage(int damage)
+    {
+        damage = 100;
+
+        // Damage the character
+        currentHealth -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage.");
+
+        // If health reaches zero
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        // Die in some way
+        // This method is meant to be overwritten
+        Debug.Log(transform.name + " died.");
+
+        //Player needs to be re-transported to last scene if re-trying the level.
+    }
+}
---
+++ CPI211/Assets/Scripts/Stats/Stat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat : MonoBehaviour
+{
+    [SerializeField]
+    private int baseValue;  // Starting value
+
+    // List of modifiers that change the baseValue
+    private List<int> modifiers = new List<int>();
+
+    // Get the final value after applying modifiers
+    public int GetValue()
+    {
+        int finalValue = baseValue;
+        modifiers.ForEach(x => finalValue += x);
+        return finalValue;
+    }
+
+    // Add new modifier
+    public void AddModifier(int modifier)
+    {
+        if (modifier != 0)
+            modifiers.Add(modifier);
+    }
+
+    // Remove a modifier
+    public void RemoveModifier(int modifier)
+    {
+        if (modifier != 0)
+            modifiers.Remove(modifier);
+    }
+
+}
