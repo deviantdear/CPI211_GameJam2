2f05fdcc76e6211b61acb2db0de378e7f33f752b
Valarie Adams <deviantdear@gmail.com>
2019-02-11 10:27:21 -0700

    Added simple Menu Script, tsting functionality of different code for the enemy and player

---
+++ CPI211/Assets/Scripts/SimpleMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleMenu : MonoBehaviour
+{
+    public GameObject startMenu;
+    public GameObject gameGUI;
+    public GameObject endMenu;
+
+    public void startGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadLevel()
+    {
+       //determine the level the player is on, re-spawn at that level 
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+   
+}
