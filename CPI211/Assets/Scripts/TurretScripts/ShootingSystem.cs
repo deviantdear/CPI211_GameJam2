5ab4dd28b8ade1d34c6ad44626590a7c06896900
Valarie Adams <deviantdear@gmail.com>
2019-02-12 11:11:41 -0700

    Adding Turret and Projectile Scripts

---
+++ CPI211/Assets/Scripts/TurretScripts/ShootingSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingSystem : MonoBehaviour
+{
+    public float fireRate;
+    public float fieldOfview;
+    public bool beam;
+    public List<GameObject> projectileSpawns;
+    public GameObject projectile;
+
+    List<GameObject> m_lastProjectile = new List<GameObject>();
+    float m_fireTimer = 0.0f;
+
+    void Update()
+    {
+        
+    }
+
+    void SpawnProjectiles()
+    {
+        if (!projectile)
+        {
+            return;
+        }
+        for(int i = 0; i < projectileSpawns.Count; i++)
+        {
+            if (projectileSpawns[i])
+            {
+
+            }
+        }
+    }
+}
